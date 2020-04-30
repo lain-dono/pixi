d68e0bc1032d0894f9f257b434d95871ee278161
Lain-dono <lain.dono@gmail.com>
2020-04-30 10:12:41 +0300

    add blend.rs

--- src/blend.rs
+++ src/blend.rs
@@ -1,88 +1,64 @@
-/*
-pub enum BlendMode {
-    Normal = 0,
-    Add = 1,
-    Multiply = 2,
-    Screen = 3,
-    Overlay = 4,
-    Darken = 5,
-    Lighten = 6,
-    ColorDodge = 7,
-    ColorBurn = 8,
-    HardLight = 9,
-    SoftLight = 10,
-    Difference = 11,
-    Exclusion = 12,
-    Hue = 13,
-    Saturation = 14,
-    Color = 15,
-    Luminosity = 16,
-    NormalNpm = 17,
-    AddNpm = 18,
-    ScreenNpm = 19,
-    None = 20,
+use wgpu::BlendFactor::{DstAlpha, One, SrcAlpha, Zero};
+use wgpu::BlendFactor::{DstColor, OneMinusSrcColor};
+use wgpu::BlendFactor::{OneMinusDstAlpha, OneMinusSrcAlpha};
 
-    SrcOver = 0,
-    SrcIn = 21,
-    SrcOut = 22,
-    SrcAtop = 23,
-    DstOver = 24,
-    DstIn = 25,
-    DstOut = 26,
-    DstAtop = 27,
-    Erase = 26,
-    Subtract = 28,
-    Xor = 29,
+pub const REPLACE: Blend = Blend {
+    alpha: wgpu::BlendDescriptor::REPLACE,
+    color: wgpu::BlendDescriptor::REPLACE,
+};
+
+pub const PMA_NORMAL: Blend = PMA_SRC_OVER;
+
+pub const PMA_SRC: Blend = Blend::add(One, Zero);
+pub const PMA_SRC_ATOP: Blend = Blend::add(DstAlpha, OneMinusSrcAlpha);
+pub const PMA_SRC_OVER: Blend = Blend::add(One, OneMinusSrcAlpha);
+pub const PMA_SRC_IN: Blend = Blend::add(DstAlpha, Zero);
+pub const PMA_SRC_OUT: Blend = Blend::add(OneMinusDstAlpha, Zero);
+
+pub const PMA_DST: Blend = Blend::add(Zero, One);
+pub const PMA_DST_ATOP: Blend = Blend::add(OneMinusDstAlpha, SrcAlpha);
+pub const PMA_DST_OVER: Blend = Blend::add(OneMinusDstAlpha, One);
+pub const PMA_DST_IN: Blend = Blend::add(Zero, SrcAlpha);
+pub const PMA_DST_OUT: Blend = Blend::add(Zero, OneMinusSrcAlpha);
+
+pub const PMA_XOR: Blend = Blend::add(OneMinusDstAlpha, OneMinusSrcAlpha);
+pub const PMA_CLEAR: Blend = Blend::add(Zero, Zero);
+
+pub const PMA_ADD: Blend = Blend::add(One, One);
+pub const PMA_MUL: Blend = Blend::add_sep(DstColor, OneMinusSrcAlpha, One, OneMinusSrcAlpha);
+pub const PMA_SCREEN: Blend = Blend::add_sep(One, OneMinusSrcColor, One, OneMinusSrcAlpha);
+
+pub const NPM_NORMAL: Blend = Blend::add_sep(SrcAlpha, OneMinusSrcAlpha, One, OneMinusSrcAlpha);
+pub const NPM_ADD: Blend = Blend::add_sep(SrcAlpha, One, One, One);
+pub const NPM_SCREEN: Blend = Blend::add_sep(SrcAlpha, OneMinusSrcColor, One, OneMinusSrcAlpha);
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct Blend {
+    pub alpha: wgpu::BlendDescriptor,
+    pub color: wgpu::BlendDescriptor,
 }
 
-pub enum CompositingMode {
-    Src,
-    SrcOver,
-    SrcIn,
+impl Blend {
+    const fn add(src: wgpu::BlendFactor, dst: wgpu::BlendFactor) -> Self {
+        Self::add_sep(src, dst, src, dst)
+    }
 
-    Dst,
-    DstOver,
-    DstIn,
-
-    Clear,
-    SrcOut,
-    SrcIn,
-}
-*/
-
-pub const NORMAL_PREMULTIPLY: BlendState = BlendState::new(
-    wgpu::BlendFactor::One,
-    wgpu::BlendFactor::OneMinusSrcAlpha,
-    wgpu::BlendOperation::Add,
-);
-
-pub const NORMAL_UNPREMULTIPLY: BlendState = BlendState::new(
-    wgpu::BlendFactor::SrcAlpha,
-    wgpu::BlendFactor::OneMinusSrcAlpha,
-    wgpu::BlendOperation::Add,
-);
-
-pub struct BlendState {
-    pub alpha_blend: wgpu::BlendDescriptor,
-    pub color_blend: wgpu::BlendDescriptor,
-}
-
-impl BlendState {
-    const fn new(
-        src_factor: wgpu::BlendFactor,
-        dst_factor: wgpu::BlendFactor,
-        operation: wgpu::BlendOperation,
+    const fn add_sep(
+        color_src: wgpu::BlendFactor,
+        color_dst: wgpu::BlendFactor,
+        alpha_src: wgpu::BlendFactor,
+        alpha_dst: wgpu::BlendFactor,
     ) -> Self {
         Self {
-            alpha_blend: wgpu::BlendDescriptor {
-                src_factor,
-                dst_factor,
-                operation,
+            alpha: wgpu::BlendDescriptor {
+                src_factor: alpha_src,
+                dst_factor: alpha_dst,
+                operation: wgpu::BlendOperation::Add,
             },
-            color_blend: wgpu::BlendDescriptor {
-                src_factor,
-                dst_factor,
-                operation,
+            color: wgpu::BlendDescriptor {
+                src_factor: color_src,
+                dst_factor: color_dst,
+                operation: wgpu::BlendOperation::Add,
             },
         }
     }
